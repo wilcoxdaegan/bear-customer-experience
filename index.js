688a352164000fcde591e823e9f69ecf25b64ad0
wilcoxdaegan <wilcoxdaegan@gmail.com>
2021-09-19 15:46:13 -0400

    initiate express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, function () {
+    console.log("Listening!");
+});
+

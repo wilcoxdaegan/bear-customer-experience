15b77c9cfab4342f4721608d98b0284e1f6e6f39
wilcoxdaegan <wilcoxdaegan@gmail.com>
2021-10-02 20:32:25 -0400

    add private

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000; // process.env.PORT for heroku, 3000 for local
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "index.html");
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.get("/private", function (req, res) { // when a person arrives at the main url + / (so, the home), it makes a request, and sends a response. 
-    res.sendFile(__dirname + "/index.html"); // sends our html file in a static way
+    res.sendFile(__dirname + "/private.html"); // sends our html file in a static way
 });
 
 app.get("/daegan", function (req, res) {
